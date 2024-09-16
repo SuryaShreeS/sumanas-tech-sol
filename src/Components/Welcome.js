8b915d5e6af7dca5f2c4ad1f62bc33a76bc61717
SuryaShreeS <gsdsuryashree@gmail.com>
2024-09-15 20:22:35 -0700

    adding styles

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,48 +1,32 @@
-// import React from "react";
-// import { useSelector } from "react-redux";
-// import { useAuth0 } from "@auth0/auth0-react";
-
-// const Welcome = () => {
-//   const { logout } = useAuth0();
-//   const user = useSelector((state) => state.auth.user);
-//   console.log(user,"User")
-
-
-
-//   return (
-//     <div>
-//       <h2>Welcome, {user?.name}!</h2>
-//       <p>Email: {user?.email}</p>
-//       <img src={user?.picture} alt="User Profile" />
-//       <button onClick={() => logout({ returnTo: window.location.origin })}>
-//         Log out
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Welcome;
 // src/components/Welcome.js
 
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import '../styles/WelcomeStyles.css'; // Import the CSS file
 
 const Welcome = () => {
   const { accessToken, email } = useSelector((state) => state.auth);
+  const { logout } = useAuth0();
   const navigate = useNavigate();
 
   React.useEffect(() => {
     if (!accessToken) {
-      navigate('/login'); // Redirect if not logged in
+      navigate('/login');
     }
   }, [accessToken, navigate]);
 
   return (
     <div className="welcome-container">
-      <h1>Welcome!</h1>
+      <h1>Welcome to Sumanas Technology!</h1>
       {email ? (
-        <p>Logged in as: {email}</p>
+        <div>
+          <p>Logged in as: {email}</p>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>
+            Log out
+          </button>
+        </div>
       ) : (
         <p>Loading user information...</p>
       )}
